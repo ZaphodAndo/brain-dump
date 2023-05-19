1f72c0d73c52fe340e72c23f78cd177faf3d8ac7
Ethan <ethan.anderson00@gmail.com>
2023-05-19 22:18:45 +0100

    Added new icons

---
+++ app/Icons/plusIcon.tsx
@@ -0,0 +1,10 @@
+export default function PlusIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none">
+      <path
+        fill="#000"
+        d="M6.75.75a.75.75 0 00-1.5 0v4.5H.75a.75.75 0 000 1.5h4.5v4.5a.75.75 0 101.5 0v-4.5h4.5a.75.75 0 100-1.5h-4.5V.75z"
+      ></path>
+    </svg>
+  );
+}
